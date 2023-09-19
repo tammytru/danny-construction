ccabe8e69f133d8d3f72fc6bcaa088c159844bc3
Tammy Truong <tammytru000@gmail.com>
2023-09-18 22:11:17 -0700

    added custom colors and text sizes. changed bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#f3f4f6',
+        'grey': {
+          1: '#f3f4f6',
+          2: '#f3f4f6',
+          3: '#e9e9e9'
+        },
+        'light-blue': '#3e8de3'
+      },
+      fontSize: {
+        'xs': '1em',
+        'sm': '2em',
+        'md': '3em',
+        'lg': '4em',
+        'xl': '5em' 
+      }
+    },
   },
   plugins: [],
 }
-
